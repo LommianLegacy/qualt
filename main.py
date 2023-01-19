7aa3263b8a96f3176f46e24f822eb05f6cf180d4
LommianLegacy <eggdogerat52@gmail.com>
2023-01-19 14:54:03 -0800

    for item in ["a","s","d","f","g"]:     print(item) for ranger in range(0,11):     print(ranger) for day in ["Monday","Tuesday","Wensday","Thursday","Friday","Saturday","Sunday"]:     print(day) for walt in range(0,6):     print(walt) for alt in range(10,16):     print(alt) for at in range(0,9,2):     print(at)

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-for item in ["a","s","d","f","g","hello"]:
+for item in ["a","s","d","f","g"]:
     print(item)
 for ranger in range(0,11):
     print(ranger)
 for day in ["Monday","Tuesday","Wensday","Thursday","Friday","Saturday","Sunday"]:
     print(day)
+for walt in range(0,6):
+    print(walt)
+for alt in range(10,16):
+    print(alt)
+for at in range(0,9,2):
+    print(at)
