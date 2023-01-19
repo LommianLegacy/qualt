7511551e033ec1105b99b4a51e47416b79db00f1
LommianLegacy <eggdogerat52@gmail.com>
2023-01-19 14:49:51 -0800

    j

---
+++ main.py
@@ -0,0 +1,6 @@
+for item in ["a","s","d","f","g","hello"]:
+    print(item)
+for ranger in range(0,11):
+    print(ranger)
+for day in ["Monday","Tuesday","Wensday","Thursday","Friday","Saturday","Sunday"]:
+    print(day)
